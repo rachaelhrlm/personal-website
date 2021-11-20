721c792f90300073773c1754106fe43a30dfd719
Rachael Morris <rachael.morris@sky.uk>
2021-11-20 19:16:06 +0000

    add breakpoints and media mixins

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,18 @@ const App: FC = () => {
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className={`app ${theme}-theme`}>
-        <Title>Hello World,</Title>
-        <SubTitle>{`I'm Rachael.`}</SubTitle>
-        <div className="app-description">
-          <Text>{`I'm an associate software developer at Sky. I started my coding journey through Sky's Get Into Tech program of 2020.`}</Text>
+        <div className="portfolio">
+          <Title>Hello World,</Title>
+          <SubTitle>{`I'm Rachael.`}</SubTitle>
+          <div className="app-description">
+            <Text>{`I'm an associate software developer at Sky. I started my coding journey through Sky's Get Into Tech program of 2020.`}</Text>
+          </div>
+          <div className={`${theme}-accent app-icons`}>
+            <Icon type={<GitHub />} href="https://github.com/rachaelhrlm" />
+            <Icon type={<Linkedin />} href="https://www.linkedin.com/in/helen-rachael-malinowska-14b4581b4/" />
+          </div>
+          <Toggle value={isDarkTheme} onChange={() => toggleTheme()} />
         </div>
-        <div className={`${theme}-accent app-icons`}>
-          <Icon type={<GitHub />} href="https://github.com/rachaelhrlm" />
-          <Icon type={<Linkedin />} href="https://www.linkedin.com/in/helen-rachael-malinowska-14b4581b4/" />
-        </div>
-        <Toggle value={isDarkTheme} onChange={() => toggleTheme()} />
       </div>
     </ThemeContext.Provider>
   );
